872e6bb842a684fe1d5ccb58ec8debfb7aa1d51d
Eric Batiste <ericbatiste4@gmail.com>
2025-03-08 12:33:01 -0700

    Simplify deployment files.

--- .platform/hooks/postdeploy/01_restart_puma.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-systemctl restart puma.service || true
--- config/puma.rb
+++ config/puma.rb
@@ -28,9 +28,7 @@ workers ENV.fetch("WEB_CONCURRENCY", 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
-preload_app!
-
-port ENV.fetch("PORT", 3000)
+port ENV.fetch("PORT") { 3000 }
 
 environment ENV.fetch("RAILS_ENV") { "production" }
 
